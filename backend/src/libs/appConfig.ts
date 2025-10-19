38647ebab912c4caa289c8b358f19037da2539b0
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-10-19 21:15:16 +0900

    env setting

--- backend/src/libs/appConfig.ts
+++ backend/src/libs/appConfig.ts
@@ -11,6 +11,19 @@ const appConfig = {
   port: Number(getEnv("APP_PORT", "8080")),
   shortSHA: getEnv("SHORT_SHA", "local"),
   buildTime: getEnv("BUILD_TIME", new Date().toISOString()),
+  corsOrigin: getEnv("CORS_ORIGIN"),
+  cognitoUserPoolId: getEnv("COGNITO_USER_POOL_ID"),
+  cognitoClientId: getEnv("COGNITO_CLIENT_ID"),
 };
+
+/**
+ * アプリが「本番環境（production）」として動いているかどうかを判定
+ */
+export const isProduction = appConfig.nodeEnv === "production";
+
+/**
+ * アプリが「開発環境（development）」として動いているかどうかを判定
+ */
+export const isDevelopment = appConfig.nodeEnv === "development";
 
 export default appConfig;
